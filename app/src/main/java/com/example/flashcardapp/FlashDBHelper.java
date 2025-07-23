a5a0d8fa5c6fe19050e48b7d7a9d061d12b72b4d
Ian McIntosh <ianamcintosh108@gmail.com>
2025-07-23 12:11:06 -0400

    commit1

---
+++ app/src/main/java/com/example/flashcardapp/FlashDBHelper.java
@@ -0,0 +1,13 @@
+//package com.example.flashcardapp;
+//
+//import android.database.sqlite.SQLiteOpenHelper;
+//
+//public class FlashDBHelper extends SQLiteOpenHelper {
+//
+//    private static final String DATABASE_NAME = "Flash.db";
+//
+//    private static final int DATABASE_VERSION = 2;
+//
+//    private static final String CREATE_TABLE_FLASH =
+//            "create table flash (_id"
+//}
