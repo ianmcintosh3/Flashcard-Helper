ace32720429da9dca6d885f24fa2dca8891ac20e
Ian McIntosh <ianamcintosh108@gmail.com>
2025-07-31 11:12:20 -0400

    work on ReviewCardActivity

--- app/src/main/java/com/example/flashcardapp/ReviewCardActivity.java
+++ app/src/main/java/com/example/flashcardapp/ReviewCardActivity.java
@@ -1,7 +1,23 @@
 package com.example.flashcardapp;
 
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ReviewCardActivity extends AppCompatActivity {
+    ArrayList<Flashcard> flashcardArrayList;
+
+    private FlashAdapter flashAdapter;
+
+    private View.OnClickListener onSubjectClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            RecyclerView.ViewHolder viewHolder = (RecyclerView.ViewHolder) v.getTag();
+            int position = viewHolder.getAdapterPosition();
+        }
+    };
 
 }
